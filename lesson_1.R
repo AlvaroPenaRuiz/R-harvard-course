19da0f8f2077620d4002792b714003c53ef67a42
Álvaro Pena Ruiz <alvaropenaruiz@gmail.com>
2024-01-29 19:10:00 +0100

    lesson 1 R

---
+++ lesson_1.R
@@ -0,0 +1,19 @@
+# Intro to R Lesson
+
+# Interacting with R
+
+## I am adding 3 and 5. R is fun!
+3+5
+
+# Sum
+x <- 3
+y <- 5
+
+# Showing the value
+y
+
+# Summing the value
+number <- x + y
+
+# Showing the value
+number
